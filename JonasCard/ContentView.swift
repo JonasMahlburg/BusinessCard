4bace56001158b3327912440d9ec60a2e25668b3
JonasMahlburg <darrylmanga@yahoo.com>
2026-07-30 11:15:32 +0200

    feat: set first Look - set custom Background - added Image - added Phonenumber -implement Custom Font (VT323-Regular)

--- JonasCard/ContentView.swift
+++ JonasCard/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.06, green: 0.73, blue: 0.69, opacity: 1.00)
+                .ignoresSafeArea()
+            VStack {
+                Image("jonas")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .clipShape(.circle)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Jonas Mahlburg")
+                    .font(.custom("VT323-Regular", size: 40))
+                Text("iOS Fullstack Developer")
+                Divider()
+                
+                RoundedRectangle(cornerRadius: 40)
+                    .fill(.white)
+                    .frame(height: 50)
+                    .overlay(HStack{
+                        Image(systemName: "phone.fill")
+                            .foregroundColor(.green)
+                        Text("+49 176 46703864")
+                    })
+   
+            }
+            .padding()
         }
-        .padding()
+
     }
 }
 
