60e9e6cd0c8888b8fa98fbbb27c4874f64d97544
JonasMahlburg <darrylmanga@yahoo.com>
2026-07-30 09:53:26 +0200

    Initial Commit

---
+++ JonasCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JonasCard
+//
+//  Created by Jonas Mahlburg on 30.07.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JonasCard/JonasCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JonasCardApp.swift
+//  JonasCard
+//
+//  Created by Jonas Mahlburg on 30.07.26.
+//
+
+import SwiftUI
+
+@main
+struct JonasCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
